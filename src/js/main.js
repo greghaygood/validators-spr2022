46059a7554f68c744a1471d1ee09fb2a51666e94
Greg Haygood <ghaygood@gmail.com>
2022-05-12 19:13:05 -0400

    initial version of validators

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,33 @@
 (function() {
-    const limitFields = document.querySelectorAll("[data-limit]");
-    limitFields.forEach((element) => {
-        new CharLimiter(element);
-    })
+    // const limitFields = document.querySelectorAll("[data-limit]");
+    // limitFields.forEach((element) => {
+    //     new CharLimiter(element);
+    // })
+    new Validations()
 }());
+
+
+// examples of inheritance
+
+class BaseClass {
+    constructor(options = {}) {
+        // console.log("BaseClass()", options)
+        this.print("BaseClass()", options)
+    }
+
+    print(...message) {
+        //console.log(...message)
+    }
+}
+
+// const bc = new BaseClass()
+
+class ChildClass1 extends BaseClass {
+    constructor(options = {}) {
+        super(options)
+        // console.log("ChildClass1", options)
+        this.print("ChildClass1", options)
+    }
+}
+
+// const cc1 = new ChildClass1()
